0e3e90d91b47ad1b5ce085b1b6b510d9997940ca
Konstantin Spirin <konstmsu@gmail.com>
2019-09-10 16:40:44 +0800

    working

--- src/board.test.js
+++ src/board.test.js
@@ -3,5 +3,6 @@ const board = require("./board");
 describe("board", () => {
   it("should return neighbours", () => {
     let b = board.create_board();
+    expect(b.count_neighbours()).toEqual(undefined);
   });
 });
