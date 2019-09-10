3b6a530c71c751d6b23d12e565b112508bf9cf52
Terry Yin <terry.yinzhe@gmail.com>
2019-09-10 16:33:53 +0800

    work

--- src/gameoflife.js
+++ src/gameoflife.js
@@ -1,3 +1,5 @@
+const { count_neighbours } = require("./countNeighbours");
+
 const game_of_life = list_of_positions => {
   if(list_of_positions != null){
       list_of_positions.map(item => console.log(item));
