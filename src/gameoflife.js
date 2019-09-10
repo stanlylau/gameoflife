c4bde145fb842e377340132818997e88001c6499
Terry Yin <terry.yinzhe@gmail.com>
2019-09-10 16:26:55 +0800

    work

--- src/gameoflife.js
+++ src/gameoflife.js
@@ -1,5 +1,7 @@
 const game_of_life = list_of_positions => {
-  list_of_positions.forEach(()=> {
+  list_of_positions.forEach((pos)=> {
+    if (count_neighbours(pos, list_of_positions) == 2) {
+    }
   });
     if(list_of_positions != null){
         list_of_positions.map(item => console.log(item));
