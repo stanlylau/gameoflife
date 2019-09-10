557633e0bf6750e1876551f01d615235b24e25ba
Saloni Kaur <missa4892@gmail.com>
2019-09-10 16:47:58 +0800

    working

--- src/countNeighbours.js
+++ src/countNeighbours.js
@@ -1,6 +1,10 @@
 function count_neighbours(thisCell, all_alive_positions) {
-  for (p in all_alive_positions) {
-  }
+    const thisCellX = thisCell[0];
+    const thisCellY = thisCell[1];
+    for (position in all_alive_positions) {
+        const Xposition = position[0];
+        const Yposition = position[1];
+    }
   return 42;
 }
 
