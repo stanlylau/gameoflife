9f7c1fa0ff22ee85bd9c6d56775bc65d2a1f9b43
Konstantin Spirin <konstmsu@gmail.com>
2019-09-10 16:49:34 +0800

    working

--- src/board.js
+++ src/board.js
@@ -1,11 +1,14 @@
 function create_board(width, height) {
   const grid = Array(height);
   for (let i = 0; i < height; i++) {
-    grid[i] = Array(width);
+    grid[i] = Array(width).fill(false);
   }
   return {
-    get_neighbours: function(cell) {
+    get_neighbours: function(xy) {
       return [];
+    },
+    set_cell: function(xy, isAlive) {
+      grid[xy[1]][xy[0]] = isAlive;
     }
   };
 }
