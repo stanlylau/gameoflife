72594bf5d7014959030d870bd0b687b8f8d45cf2
Saloni Kaur <missa4892@gmail.com>
2019-09-10 16:02:57 +0800

    working

--- src/gameoflife.js
+++ src/gameoflife.js
@@ -1,3 +1,10 @@
 const game_of_life = (a_list_of_alive_position) => {}
 
 module.export = game_of_life;
+
+var a_list_of_positions = [
+    [1, 2],
+    [3, 4],
+    [5, 6]
+  ];
+game_of_life(a_list_of_positions);
