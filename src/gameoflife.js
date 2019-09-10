f3b02f131b1d36a6657ea88553af043d0809da76
Konstantin Spirin <konstmsu@gmail.com>
2019-09-10 16:38:48 +0800

    working

--- src/gameoflife.js
+++ src/gameoflife.js
@@ -1,17 +1,16 @@
 const { count_neighbours, neighbours } = require("./countNeighbours");
 
-
 const game_of_life = list_of_positions => {
   var result = [];
-  if(list_of_positions != null){
-      list_of_positions.map(pos => {
-        if( 2===count_neighbours(pos, list_of_positions)) {
-          result.push(pos);
-        }
-        if( 3===count_neighbours(pos, list_of_positions)) {
-          result.push(pos);
-        }
-      });
+  if (list_of_positions != null) {
+    list_of_positions.map(pos => {
+      if (2 === count_neighbours(pos, list_of_positions)) {
+        result.push(pos);
+      }
+      if (3 === count_neighbours(pos, list_of_positions)) {
+        result.push(pos);
+      }
+    });
   }
 
   return result;
