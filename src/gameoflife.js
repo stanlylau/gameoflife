acc86362a318315d8a6068f7becb232f32db1cd4
Konstantin Spirin <konstmsu@gmail.com>
2019-09-10 16:09:56 +0800

    working

--- src/gameoflife.js
+++ src/gameoflife.js
@@ -1,10 +1,10 @@
 const game_of_life = board => {};
 
-module.exports = { game_of_life };
-
 var a_list_of_positions = [[1, 2], [3, 4], [5, 6]];
 game_of_life(a_list_of_positions);
 
 function count_neighbours(center, all_alive_positions) {
   return 42;
 }
+
+module.exports = { game_of_life };
