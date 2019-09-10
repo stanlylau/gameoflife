14575ac43bd12c3ce776af6dde6980bec975e38a
Konstantin Spirin <konstmsu@gmail.com>
2019-09-10 16:07:05 +0800

    working

--- src/gameoflife.test.js
+++ src/gameoflife.test.js
@@ -1,6 +1,11 @@
-const { game_of_life } = require('./gameoflife');
+const { game_of_life } = require("./gameoflife");
 
-it("abc", ()=> {
+it("abc", () => {
   expect(1).toEqual(1);
 });
 
+describe("count neighbours", () => {
+  it("empty", () => {
+    //game_of_life.count_neighbours
+  });
+});
