c73a79c70489ffb182907b03dc690831ca85df59
Sergey Shishkin <sergei.shishkin@gmail.com>
2019-09-10 16:31:28 +0800

    working

--- src/gameoflife.js
+++ src/gameoflife.js
@@ -1,11 +1,7 @@
 const game_of_life = list_of_positions => {
-  list_of_positions.forEach((pos)=> {
-    if (count_neighbours(pos, list_of_positions) == 2) {
-    }
-  });
-    if(list_of_positions != null){
-        list_of_positions.map(item => console.log(item));
-    }
+  if(list_of_positions != null){
+      list_of_positions.map(item => console.log(item));
+  }
 };
 
 // TODO: What Happens to Cell?
